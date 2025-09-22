089bea8f431a8b595f4d0ae323ba1938b8aabc0c
Nick Ng <ngyewch@gmail.com>
2025-09-22 19:47:25 +0800

    feat: data type sizes

--- dataTypes.go
+++ dataTypes.go
@@ -26,3 +26,40 @@ const (
 	DataTypeComplexDoubleFloat    = 0x10000d
 	DataTypeDAQmxRawData          = 0xffffffff
 )
+
+func (dataType DataType) SizeOf() int {
+	switch dataType {
+	case DataTypeVoid:
+		return 0
+	case DataTypeI8:
+		return 1
+	case DataTypeI16:
+		return 2
+	case DataTypeI32:
+		return 4
+	case DataTypeI64:
+		return 8
+	case DataTypeU8:
+		return 1
+	case DataTypeU16:
+		return 2
+	case DataTypeU32:
+		return 4
+	case DataTypeU64:
+		return 8
+	case DataTypeSingleFloat:
+		return 4
+	case DataTypeDoubleFloat:
+		return 8
+	case DataTypeBoolean:
+		return 1
+	case DataTypeTimestamp:
+		return 16
+	case DataTypeComplexSingleFloat:
+		return 8
+	case DataTypeComplexDoubleFloat:
+		return 16
+	default:
+		return -1
+	}
+}
